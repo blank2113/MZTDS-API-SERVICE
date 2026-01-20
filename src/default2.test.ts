fa42ead29b13c1f590886deb9770c392c5e46588
blank2113 <blank2113@github.com>
2026-01-20 21:55:00 +0500

    try to check branch guard rules5

--- src/default2.test.ts
+++ src/default2.test.ts
@@ -2,12 +2,12 @@ import request from "supertest";
 import { describe, it, expect } from "vitest";
 import { app } from "./app.js";
 
-// Default test3
+// Default test4
 describe("Default test2", () => {
   it("Get default route2", async () => {
     const res = await request(app).get("/health");
 
-    expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty("message");
+    expect(res.status).toBe(500);
+    // expect(res.body).toHaveProperty("message");
   });
 });
