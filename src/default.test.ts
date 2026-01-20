44df25f230cead9673b33c1c58a9574630e1b70b
blank2113 <blank2113@github.com>
2026-01-20 20:24:27 +0500

    fix

--- src/default.test.ts
+++ src/default.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { describe, it, expect } from "vitest";
 import { app } from "./app.js";
 
+// Default test
 describe("Default test", () => {
   it("Get default route", async () => {
     const res = await request(app).get("/");
