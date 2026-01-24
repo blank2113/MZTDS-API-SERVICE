c6e07bbfec7062c679dba7307af12b3e24ba9a9b
blank2113 <blank2113@github.com>
2026-01-24 16:35:23 +0500

    cookies3

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       maxAge: 1000 * 60 * 60 * 24, // 1 день
       sameSite: "lax",
     },
