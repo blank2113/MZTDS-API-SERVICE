fad30a41df146838b01ba939eed447b894f307f3
Evgeniy Em <103075633+blank2113@users.noreply.github.com>
2026-01-24 16:33:36 +0500

    Cookies2 (#20)
    * cookies
    
    * cookies2
    
    ---------
    
    Co-authored-by: blank2113 <blank2113@github.com>

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(
 
 app.use(compression());
 app.use(express.json({ limit: "20mb" }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(`${process.env.PREFIX}`, routes);
 const openApiDocument = generateOpenApiDocument();
