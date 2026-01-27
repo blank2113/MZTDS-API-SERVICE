45fba5e7a11fc074af6b494de53df03a1581aebb
Evgeniy Em <103075633+blank2113@users.noreply.github.com>
2026-01-27 16:59:24 +0500

    Fix redis5 (#37)
    * fix redis problem
    
    * fix redis problem5
    
    ---------
    
    Co-authored-by: blank2113 <blank2113@github.com>

--- src/queues/mailing.worker.ts
+++ src/queues/mailing.worker.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Worker } from "bullmq";
 import { prisma } from "../lib/prisma.js";
 import { bot } from "../bot/bot.service.js";
+import { redisOptions } from "./mailing.queue.js";
 
 export const mailingWorker = new Worker(
   "mailing",
@@ -21,7 +21,6 @@ export const mailingWorker = new Worker(
     } catch (err: any) {
       console.error(`❌ Failed to send message to ${telegram_id}:`, err);
 
-      // Если пользователь заблокировал бота
       if (err?.code === 403) {
         await prisma.user.updateMany({
           where: { telegram_id: String(telegram_id) },
@@ -30,12 +29,11 @@ export const mailingWorker = new Worker(
         console.log(`⚠️ User ${telegram_id} unsubscribed`);
       }
 
-      throw err; // BullMQ попробует retry
+      throw err;
     }
   },
   {
-    // @ts-expect-error
-    connection: process.env.REDIS_URL,
+    connection: redisOptions,
     concurrency: 5,
     limiter: { max: 20, duration: 1000 },
   },
