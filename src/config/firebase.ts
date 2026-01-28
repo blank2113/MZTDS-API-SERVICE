77e0be0c772bd6a9d1734ecab18567bd017bad0e
blank2113 <blank2113@github.com>
2026-01-28 21:01:25 +0500

    push notifications & inivite service

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON!);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
 
 if (!admin.apps.length) {
   admin.initializeApp({
