375193996480ae2b06f80ffe6587a3ddfc987100
Evgeniy Em <103075633+blank2113@users.noreply.github.com>
2026-01-29 17:33:31 +0500

    fix logger (#44)
    * fix logger
    
    * fix logger
    
    ---------
    
    Co-authored-by: blank2113 <blank2113@github.com>

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,7 @@ app.use(compression());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(`${process.env.PREFIX}`, routes);
+app.use(requestLogger);
 app.use(
   "/docs",
   swaggerUi.serve,
@@ -48,6 +49,5 @@ app.use(
     },
   }),
 );
-app.use(requestLogger);
 
 app.use(errorHandler);
